58d21491790c5c9d57e31960a4d50af7d45d53ab
wngur6076 <wngur6076@naver.com>
2020-12-26 19:09:41 +0900

    새상품 추가

--- app.js
+++ app.js
@@ -199,6 +199,13 @@ new Vue({
       perPage: 10,
 
       currentPage: 1,
+
+      product: {
+         id: null,
+         name: '',
+         category: '',
+         price: ''
+      }
    },
    computed: {
       productsPaginated() {
@@ -253,6 +260,25 @@ new Vue({
       }
    },
    methods: {
+      save () {
+         if (this.product.name && this.product.category && this.product.price) {
+            this.product.id = this.products.length + 1
+
+            this.products.unshift(this.product)
+
+            this.product = {
+               id: null,
+               name: '',
+               category: '',
+               price: '' 
+            }
+
+            $(this.$refs.vuemodal).modal('hide');
+         } else {
+            alert("Please fill in the form properly")
+         }
+      },
+
       switchPage (page) {
          this.currentPage = page
       },
